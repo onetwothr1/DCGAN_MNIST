70d2eb03bc306840c5539f01da70d7ce2295406a
onetwothr1 <kimjh1101@snu.ac.kr>
2023-05-26 12:38:38 +0900

    Initial commit

---
+++ train.py
@@ -0,0 +1,122 @@
+import torch
+import torch.optim as optim
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from model import Generator, Discriminator
+from utils import *
+
+
+class Train():
+    def __init__(self, generator, discriminator, d_noise):
+        self.G = generator
+        self.D = discriminator
+        self.optim_g = optim.Adam(self.G.parameters(), lr = 0.0002)
+        self.optim_d = optim.AdamD(self.D.parameters(), lr = 0.0002)
+        self.d_noise = d_noise
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        self.dataloader()
+
+        self.fixed_noise = torch.randn(opt.num_test_samples, 100, 1, 1, device=self.device)
+
+    def dataloader(self):
+        standardizator = transforms.Compose([
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean=(0.5),  
+                                                        std=(0.5))])
+
+        train_data = dsets.MNIST(root='data/', train=True, transform=standardizator, download=True)
+        test_data = dsets.MNIST(root='data/', train=False, transform=standardizator, download=True)
+
+        self.batch_size = 256
+        self.train_data_loader = DataLoader(train_data, self.batch_size, shuffle=True)
+        self.test_data_loader = DataLoader(test_data, self.batch_size, shuffle=True)
+
+    def run_epoch_G(self):
+        train_loss_g = 0
+            
+        for _, _ in self.train_data_loader:
+            self.optimizer_g.zero_grad()
+
+            p_fake = self.D(self.G(sample_z(self.d_noise, self.batch_size)))
+
+            loss_g = -1 * torch.log(p_fake).mean()
+            loss_g.backward()
+            self.optim_g.step()
+
+            train_loss_g += loss_g
+
+        return train_loss_g / len(self.train_data_loader)
+    
+    def run_epoch_D(self):
+        train_loss_d = 0
+
+        for train_img, train_label in self.train_data_loader:
+            train_img, train_label = train_img.to(self.device), train_label.to(self.device)
+            
+            self.optimizer_d.zero_grad()
+            self.optimizer_g.zero_grad()
+
+            p_real = self.D(train_img.view(-1, 28*28))
+            p_fake = self.D(self.G(sample_z(self.d_noise, self.batch_size if len(train_img)==self.batch_size else len(train_img))))
+
+            loss_real = -1 * torch.log(p_real)
+            loss_fake = -1 * torch.log(1 - p_fake)
+            loss_d = (loss_real + loss_fake).mean()
+            loss_d.backward()
+            self.optim_d.step()
+
+            train_loss_d += loss_d
+
+        return train_loss_d / len(self.train_data_loader)
+
+    def evaluate(self, generator, discriminator):
+        p_real, p_fake = 0., 0.
+        for test_img, test_label in self.test_data_loader:
+            test_img, test_label = test_img.to(self.device), test_label.to(self.device)
+
+            with torch.autograd.no_grad():
+                p_real += (torch.sum(discriminator(test_img.view(-1, 28*28))).item())
+                p_fake += (torch.sum(discriminator(generator(sample_z(self.d_noise, self.batch_size)))).item())
+            
+        return p_real / len(self.test_data_loader.dataset), p_fake / len(self.test_data_loader.dataset)
+
+    def train(self, num_epoch, k):
+        train_loss_d_list = []
+        train_loss_g_list = []
+        p_real_list = []
+        p_fake_list = []
+        
+        for epoch in tqdm(range(1, num_epoch+1)):
+            for _ in range(k):
+                train_loss_d = self.run_epoch_D()
+            train_loss_g = self.run_epoch_G()
+
+            p_real, p_fake = self.evaluate()
+
+            train_loss_d_list.append(train_loss_d)
+            train_loss_g_list.append(train_loss_g)
+            p_real_list.append(p_real)
+            p_fake_list.append(p_fake)
+            
+            generate_images(epoch, '/images/', self.fixed_noise, 16, self.G, self.device, use_fixed=True)
+            
+            if(epoch % 25 == 0):
+                print('(epoch %i/200) p_real: %f, p_g: %f' % (epoch+1, p_real, p_fake))
+                imshow_grid(G(sample_z(16)).view(-1, 1, 28, 28))
+
+        return train_loss_d_list, train_loss_g_list, p_real_list, p_fake_list
+
+
+if __name__=='__main__':
+    nz = 100
+    ngf = 256
+    ndf = 256
+    nc = 1
+    G = Generator(nc, nz, ngf)
+    D = Discriminator(nc, ndf)
+    train = Train(G, D)
+    train.train(num_epoch=1, k=5)
---
+++ utils.py
@@ -0,0 +1,86 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import torch
+from torchvision.utils import make_grid
+import math
+
+def plot(train_loss_d, train_loss_g, p_real, p_fake):
+  fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12,4))
+
+  ax1.plot(train_loss_d, label='D train loss')
+  ax1.plot(train_loss_g, label='G train loss')
+  ax1.set_xlabel('epoch')
+  ax1.set_xlabel('loss')
+  ax1.legend(bbox_to_anchor=(0.8, 1), loc=2, borderaxespad=0.)
+
+  ax2.plot(p_real, label='p_real')
+  ax2.plot(p_fake, label='p_fake')
+  ax2.set_xlabel('epoch')
+  ax2.set_ylabel('p')
+  ax2.legend(bbox_to_anchor=(0.9, 1), loc=2, borderaxespad=0.)
+
+  plt.show()
+
+def sample_z(d_noise, batch_size=1, device='cpu'):
+  return torch.randn(batch_size, d_noise, device=device)
+
+def imshow(img):
+    img = (img+1)/2    
+    img = img.squeeze()
+    np_img = img.numpy()
+    plt.imshow(np_img, cmap='gray')
+    plt.show()
+
+def imshow_grid(img):
+    img = make_grid(img.cpu().detach())
+    img = (img+1)/2
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1,2,0)))
+    plt.show()
+
+def generate_images(epoch, path, fixed_noise, num_test_samples, netG, device, use_fixed=False):
+    '''
+    reference: https://github.com/AKASHKADEL/dcgan-mnist
+    '''
+    z = torch.randn(num_test_samples, 100, 1, 1, device=device)
+    size_figure_grid = int(math.sqrt(num_test_samples))
+    title = None
+  
+    if use_fixed:
+        generated_fake_images = netG(fixed_noise)
+        path += 'fixed_noise/'
+        title = 'Fixed Noise'
+    else:
+        generated_fake_images = netG(z)
+        path += 'variable_noise/'
+        title = 'Variable Noise'
+  
+    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(6,6))
+    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
+        ax[i,j].get_xaxis().set_visible(False)
+        ax[i,j].get_yaxis().set_visible(False)
+    for k in range(num_test_samples):
+        i = k//4
+        j = k%4
+        ax[i,j].cla()
+        ax[i,j].imshow(generated_fake_images[k].data.cpu().numpy().reshape(28,28), cmap='Greys')
+    label = 'Epoch_{}'.format(epoch+1)
+    fig.text(0.5, 0.04, label, ha='center')
+    fig.suptitle(title)
+    fig.savefig(path+label+'.png')
+
+def save_gif(path, fps, fixed_noise=False):
+    '''
+    reference: https://github.com/AKASHKADEL/dcgan-mnist
+    '''
+    if fixed_noise==True:
+        path += 'fixed_noise/'
+    else:
+        path += 'variable_noise/'
+    images = glob(path + '*.png')
+    images = natsort.natsorted(images)
+    gif = []
+
+    for image in images:
+        gif.append(imageio.imread(image))
+    imageio.mimsave(path+'animated.gif', gif, fps=fps)
